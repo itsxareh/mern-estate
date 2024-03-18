228c666fd53f6c4859d49b11bc5df3b72e778165
Xareh <raytos.r.bsinfotech@gmail.com>
2024-03-18 22:51:42 +0800

    user image

--- Estate/api/controllers/auth.controller.js
+++ Estate/api/controllers/auth.controller.js
@@ -37,7 +37,7 @@ export const google = async (req, res, next) => {
         const user = await User.findOne({ email: req.body.email})
         if (user) {
             const token = jwt.sign({ id: user._id}, process.env.JWT_SECRET);
-            const { password: pass, ...rest} = user.doc;
+            const { password: pass, ...rest} = user._doc;
             res
             .cookie('access_token', token, { httpOnly: true} )
             .status(200)
@@ -45,10 +45,10 @@ export const google = async (req, res, next) => {
         } else {
             const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
             const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
-            const newUser = new User({ username: req.body.name.split(" ").join("").tolowerCase() + Math.random().toString(36).slice(-4), email: req.body.email, password: hashedPassword, avatar: req.body.photo});
+            const newUser = new User({ username: req.body.name.split(" ").join("").toLowerCase() + Math.random().toString(36).slice(-4), email: req.body.email, password: hashedPassword, avatar: req.body.photo});
             await newUser.save();
-            const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
-            const { password: pass, ...rest} = user.doc;
+            const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET);
+            const { password: pass, ...rest} = newUser._doc;
             res
             .cookie('access_token', token, { httpOnly: true} )
             .status(200)
